0e96f906ea60920c126dc63f58e587db2b136d6f
Denis <Hixon10@yandex.ru>
2022-09-28 01:12:18 +0100

    Implement getThreadUrlsForIndex

--- indexer/src/main/java/ru/hixon/Indexer.java
+++ indexer/src/main/java/ru/hixon/Indexer.java
@@ -1,62 +1,95 @@
 package ru.hixon;
 
-import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.UUID;
-
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 
 import static java.time.temporal.ChronoUnit.SECONDS;
 
 
 public class Indexer {
 
-    public void index() throws Exception {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://mail.openjdk.org/pipermail/amber-dev/2022-September/007456.html"))
-                .timeout(Duration.of(5, SECONDS))
-                .GET()
-                .build();
+    private static final Duration HTTP_TIMEOUT = Duration.of(5, SECONDS);
+    private static final String THREAD_SEARCH_SUBSTRING = "[ Thread ]";
 
-        HttpResponse<String> response = HttpClient.newBuilder()
-                .followRedirects(HttpClient.Redirect.ALWAYS)
-                .build()
-                .send(request, HttpResponse.BodyHandlers.ofString());
+    private final Database database;
+    private final HttpClient httpClient;
+    private final List<String> mailingListArchives;
 
-        saveToDb(response.body());
+    public Indexer(Database database, HttpClient httpClient, List<String> mailingListArchives) {
+        this.database = database;
+        this.httpClient = httpClient;
+        this.mailingListArchives = mailingListArchives;
     }
 
-    private void saveToDb(String response) {
-        Connection connection = null;
-        try {
-            // create a database connection
-            connection = DriverManager.getConnection("jdbc:sqlite:../docs/mydatabase.db");
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(10);  // set timeout to 10 sec.
+    public void index() throws Exception {
+        Set<ThreadUrlInMailingList> threadUrlsForIndex = getThreadUrlsForIndex();
 
-            statement.executeUpdate("INSERT INTO myTable (id, name) VALUES (7, '" + response.substring(0, 100) + UUID.randomUUID().toString() + "')");
-        } catch (SQLException e) {
-            // if the error message is "out of memory",
-            // it probably means no database file is found
-            System.err.println(e.getMessage());
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                // connection close failed.
-                System.err.println(e.getMessage());
-            }
+        /// TODO...
+    }
+
+    /**
+     * For each mailing list define, which months should be indexed.
+     * We need always reindex the current month, because it could change.
+     * Apart from that, we need to index months, which we have never indexed before.
+     * @return thread urls, which need to index
+     */
+    private Set<ThreadUrlInMailingList> getThreadUrlsForIndex() throws Exception {
+        List<CompletableFuture<HttpResponse<String>>> resultsCf = new ArrayList<>();
+        for (String mailingListArchive : mailingListArchives) {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(mailingListArchive))
+                    .timeout(HTTP_TIMEOUT)
+                    .GET()
+                    .build();
+            resultsCf.add(httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString()));
         }
+        CompletableFuture.allOf(resultsCf.toArray(new CompletableFuture[0])).join();
+
+        Set<ThreadUrlInMailingList> threadUrlsForIndex = new HashSet<>();
+        Set<String> indexedMonthUrls = database.getIndexedMonthUrls();
+
+        for (int i = 0; i < resultsCf.size(); i++) {
+            HttpResponse<String> archivesPageResponse = resultsCf.get(i).get();
+            String currentMailingListUrl = mailingListArchives.get(i);
+            if (archivesPageResponse.statusCode() != 200) {
+                throw new RuntimeException("Wrong HTTP code, while getting archivesPage: code=%d, link=%s".formatted(archivesPageResponse.statusCode(), currentMailingListUrl));
+            }
+            Set<ThreadUrlInMailingList> threadLinks = parseThreadLinks(archivesPageResponse.body(), indexedMonthUrls, currentMailingListUrl);
+            threadUrlsForIndex.addAll(threadLinks);
+        }
+
+        return threadUrlsForIndex;
+    }
+
+    static Set<ThreadUrlInMailingList> parseThreadLinks(String archivePageContent, Set<String> indexedMonthUrls, String currentMailingListUrl) {
+        int index = archivePageContent.indexOf(THREAD_SEARCH_SUBSTRING);
+        boolean firstAdded = false;
+        Set<ThreadUrlInMailingList> result = new HashSet<>();
+        while (index >= 0) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = index - 3; j >= 0; j--) {
+                char ch = archivePageContent.charAt(j);
+                if (ch == '"') {
+                    break;
+                }
+                sb.append(ch);
+            }
+            String threadUrl = currentMailingListUrl + sb.reverse();
+            // we want to index either the current month, or month, which we haven't seen before
+            if (!firstAdded || !indexedMonthUrls.contains(threadUrl)) {
+                result.add(new ThreadUrlInMailingList(threadUrl, currentMailingListUrl));
+            }
+            firstAdded = true;
+            index = archivePageContent.indexOf(THREAD_SEARCH_SUBSTRING, index + 1);
+        }
+        return result;
     }
 }
--- indexer/src/test/java/ru/hixon/IndexerTest.java
+++ indexer/src/test/java/ru/hixon/IndexerTest.java
@@ -1,14 +1,53 @@
 package ru.hixon;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class IndexerTest {
+public class IndexerTest {
 
     @Test
-    void indexTest() throws Exception {
-        Indexer indexer = new Indexer();
-        indexer.index();
+    public void indexTest() {
+//        Indexer indexer = new Indexer();
+//        indexer.index();
+//        new Main().run();
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void parseThreadLinksTest() throws IOException {
+        InputStream contentIs = getClass().getResourceAsStream("/archivesPage.html");
+        String html = new String(contentIs.readAllBytes(), StandardCharsets.UTF_8);
+        Set<ThreadUrlInMailingList> threadUrls = Indexer.parseThreadLinks(html, Set.of(), "https://mail.openjdk.org/pipermail/valhalla-dev/");
+
+        Assertions.assertEquals(97, threadUrls.size());
+        for (ThreadUrlInMailingList threadUrl : threadUrls) {
+            Assertions.assertEquals("https://mail.openjdk.org/pipermail/valhalla-dev/", threadUrl.getMailingListUrl());
+        }
+        Assertions.assertTrue(threadUrls.stream().anyMatch(p -> p.getThreadUrl().equals("https://mail.openjdk.org/pipermail/valhalla-dev/2022-September/thread.html")));
+        Assertions.assertTrue(threadUrls.stream().anyMatch(p -> p.getThreadUrl().equals("https://mail.openjdk.org/pipermail/valhalla-dev/2021-August/thread.html")));
+        Assertions.assertTrue(threadUrls.stream().anyMatch(p -> p.getThreadUrl().equals("https://mail.openjdk.org/pipermail/valhalla-dev/2021-May/thread.html")));
+        Assertions.assertTrue(threadUrls.stream().anyMatch(p -> p.getThreadUrl().equals("https://mail.openjdk.org/pipermail/valhalla-dev/2014-July/thread.html")));
+
+        // with filters
+        threadUrls = Indexer.parseThreadLinks(html, Set.of(
+                "https://mail.openjdk.org/pipermail/valhalla-dev/2022-September/thread.html",
+                "https://mail.openjdk.org/pipermail/valhalla-dev/2021-May/thread.html"
+        ), "https://mail.openjdk.org/pipermail/valhalla-dev/");
+
+        Assertions.assertEquals(97, threadUrls.size());
+        for (ThreadUrlInMailingList threadUrl : threadUrls) {
+            Assertions.assertEquals("https://mail.openjdk.org/pipermail/valhalla-dev/", threadUrl.getMailingListUrl());
+        }
+        Assertions.assertTrue(threadUrls.stream().anyMatch(p -> p.getThreadUrl().equals("https://mail.openjdk.org/pipermail/valhalla-dev/2022-September/thread.html")));
+        Assertions.assertTrue(threadUrls.stream().anyMatch(p -> p.getThreadUrl().equals("https://mail.openjdk.org/pipermail/valhalla-dev/2021-August/thread.html")));
+        Assertions.assertFalse(threadUrls.stream().anyMatch(p -> p.getThreadUrl().equals("https://mail.openjdk.org/pipermail/valhalla-dev/2021-May/thread.html")));
+        Assertions.assertTrue(threadUrls.stream().anyMatch(p -> p.getThreadUrl().equals("https://mail.openjdk.org/pipermail/valhalla-dev/2014-July/thread.html")));
     }
 }
